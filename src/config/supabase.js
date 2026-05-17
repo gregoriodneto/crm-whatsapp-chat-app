5dbb44ad5bf09cfe9435c7af8d949236bba96a6a
gregoriodneto <igregoriodoneto01@gmail.com>
2026-05-17 20:03:02 -0300

    fix: ajuste quanto a configuracao do supabase devido a versao do node

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,9 +1,16 @@
 const { createClient } =
   require('@supabase/supabase-js')
 
+const ws = require('ws')
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_KEY,
+  {
+    realtime: {
+      transport: ws
+    }
+  }
 )
 
 module.exports = supabase
